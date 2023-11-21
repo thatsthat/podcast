9a0d2f12c0d86f1c3bc8bc325abe6eb567851c00
Aram <aram.anto@gmail.com>
2023-11-21 14:53:44 +0100

    iterate over list of episode urls

--- index.js
+++ index.js
@@ -1,46 +1,22 @@
+import RSS from "rss";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 puppeteer.use(StealthPlugin());
 
-(async () => {
+const fetchData = async (url) => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({ headless: "new" });
   // false, slowMo: 100 });
   const page = await browser.newPage();
 
   // Navigate the page to a URL
-  await page.goto(
-    //"https://www.ccma.cat/3cat/lexili-i-el-retorn-de-teresa-pamies/audio/1187849/",
-    //"https://www.ccma.cat/3cat/en-guardia/audio/963141/",
-    "https://www.ccma.cat/3cat/32-francesc-macia/audio/102442/",
-
-    {
-      waitUntil: "domcontentloaded",
-    }
-  );
+  await page.goto(url, { waitUntil: "domcontentloaded" });
 
   // Set screen size
   await page.setViewport({ width: 1080, height: 1024 });
 
   // Click play button
   const buttonSelector = ".jw-icon-playback";
-  const cookiesSelector = "#didomi-notice-agree-button";
-  const signupSelector = ".onboarding_botoTanca__bpmHW";
-
-  /*   await Promise.all([
-    page.waitForSelector(cookiesSelector),
-    page.click(cookiesSelector),
-    page.waitForSelector(signupSelector),
-    page.click(signupSelector),
-    page.waitForSelector(buttonSelector),
-    page.click(buttonSelector),
-  ]); */
-
-  /*   await page.waitForSelector(cookiesSelector);
-  await page.click(cookiesSelector);
-
-  await page.waitForSelector(signupSelector);
-  await page.click(signupSelector); */
 
   await page.waitForSelector(buttonSelector);
   await page.click(buttonSelector);
@@ -49,18 +25,61 @@ puppeteer.use(StealthPlugin());
   const titleSelector = ".itemaudio_titol__leaYf";
   const titleNode = await page.waitForSelector(titleSelector);
   const fullTitle = await titleNode?.evaluate((el) => el.textContent);
-  console.log(fullTitle);
 
   // Read audio source
   const audioSelector = "video";
   const audioNode = await page.waitForSelector(audioSelector);
   const audioSource = await audioNode?.evaluate((el) => el.getAttribute("src"));
-  console.log(audioSource);
 
   await browser.close();
-
   const response = await fetch(audioSource, { method: "HEAD" });
-  //const content = await response.json();
-  console.log(response.headers.get("content-length"));
-  console.log(response.headers.get("last-modified"));
+
+  const obj = {
+    title: fullTitle,
+    fileURL: audioSource,
+    fileSize: response.headers.get("content-length"),
+    fileDate: response.headers.get("last-modified"),
+  };
+
+  return obj;
+};
+
+(async () => {
+  const audioURLs = [
+    "https://www.ccma.cat/3cat/3-la-primera-guerra-carlina/audio/99528/",
+    "https://www.ccma.cat/3cat/2-pirates-i-corsaris/audio/99541/",
+    "https://www.ccma.cat/3cat/1-la-batalla-dalmenar/audio/99527/",
+  ];
+
+  audioURLs.forEach(async (url) => {
+    const episode = await fetchData(url);
+    console.log(episode);
+  });
+
+  // Generate RSS feed xml file
+
+  const feed = new RSS({
+    title: "En Guàrdia!",
+    description: "A podcast about my favorite things",
+    feed_url: "https://my-podcast.com/feed.xml",
+    site_url: "https://my-podcast.com",
+  });
+
+  // Add each audio file to the RSS feed
+  /*   files.forEach((file) => {
+    const url = `https://storage.googleapis.com/your-bucket-name/${file.name}`;
+    feed.item({
+      title: file.name,
+      description: "Listen to this audio track",
+      url,
+      enclosure: {
+        url,
+        size: file.metadata.size,
+        type: "audio/mpeg",
+      },
+    });
+  }); */
+
+  // Generate the XML file
+  const xml = feed.xml();
 })();
