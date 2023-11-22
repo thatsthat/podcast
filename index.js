b9a576bfcfa3e6be8e8fdb2002ab134700c05563
Aram <aram.anto@gmail.com>
2023-11-22 15:42:06 +0100

    for loop instead of forEach

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import fs from "fs";
 import RSS from "rss";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
 puppeteer.use(StealthPlugin());
 
 const fetchData = async (url) => {
@@ -46,40 +48,52 @@ const fetchData = async (url) => {
 
 (async () => {
   const audioURLs = [
-    "https://www.ccma.cat/3cat/3-la-primera-guerra-carlina/audio/99528/",
-    "https://www.ccma.cat/3cat/2-pirates-i-corsaris/audio/99541/",
     "https://www.ccma.cat/3cat/1-la-batalla-dalmenar/audio/99527/",
+    "https://www.ccma.cat/3cat/2-pirates-i-corsaris/audio/99541/",
+    "https://www.ccma.cat/3cat/3-la-primera-guerra-carlina/audio/99528/",
   ];
 
-  audioURLs.forEach(async (url) => {
-    const episode = await fetchData(url);
-    console.log(episode);
-  });
+  const allFetches = [];
 
-  // Generate RSS feed xml file
+  /*   audioURLs.forEach(async (url) => {
+    allFetches.push(fetchData(url));
+  }); */
+
+  for (let i = 0; i < audioURLs.length; i++) {
+    allFetches.push(await fetchData(audioURLs[i]));
+  }
+
+  //const allResults = await Promise.all(allFetches);
+
+  console.log(allFetches);
+
+  /* // Generate RSS feed xml file
 
   const feed = new RSS({
     title: "En Guàrdia!",
-    description: "A podcast about my favorite things",
+    description:
+      "Tots els capítols del programa En Guàrdia, de Catalunya Ràdio",
     feed_url: "https://my-podcast.com/feed.xml",
-    site_url: "https://my-podcast.com",
+    site_url: "https://www.ccma.cat/catradio/alacarta/en-guardia/",
   });
 
   // Add each audio file to the RSS feed
-  /*   files.forEach((file) => {
-    const url = `https://storage.googleapis.com/your-bucket-name/${file.name}`;
+  allResults.forEach((ep) => {
     feed.item({
-      title: file.name,
-      description: "Listen to this audio track",
-      url,
+      title: ep.title,
       enclosure: {
-        url,
-        size: file.metadata.size,
+        url: ep.fileURL,
+        size: ep.fileSize,
         type: "audio/mpeg",
       },
     });
-  }); */
+  });
 
   // Generate the XML file
   const xml = feed.xml();
+
+  // Save it into a text file
+  var writeStream = fs.createWriteStream("enguardia.xml");
+  writeStream.write(xml);
+  writeStream.end(); */
 })();
